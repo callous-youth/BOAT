28f04b420466865ca796a75465ccc1023349a712
QHofmann <38395099+callous-youth@users.noreply.github.com>
2025-09-26 10:28:13 +0800

    chore(config): update configuration file

--- examples/data_hyper_cleaning/configs/loss_func_dhl.py
+++ examples/data_hyper_cleaning/configs/loss_func_dhl.py
@@ -1,4 +1,12 @@
+import torch
 from torch.nn import functional as F
+
+
+def loss_F(parameters):
+    loss = 0
+    for w in parameters:
+        loss += torch.linalg.norm(w) ** 2
+    return loss
 
 
 def ul_loss(ul_feed_dict, upper_model, lower_model, weights=0.0, **kwargs):
@@ -19,8 +27,19 @@ def ll_loss(ll_feed_dict, upper_model, lower_model, weights=0.0, **kwargs):
     return out
 
 
+def BOME_ll_loss(ll_feed_dict, upper_model, lower_model, weights=0.0, **kwargs):
+    out = upper_model(
+        F.cross_entropy(
+            lower_model(ll_feed_dict["data"], **kwargs),
+            ll_feed_dict["target"],
+            reduction="none",
+        ) + 0.01 * loss_F(lower_model.parameters())
+    )
+    return out
+
+
 def gda_loss(
-    ll_feed_dict, ul_feed_dict, upper_model, lower_model, weights=0.0, **kwargs
+        ll_feed_dict, ul_feed_dict, upper_model, lower_model, weights=0.0, **kwargs
 ):
     out = ll_feed_dict["alpha"] * F.cross_entropy(
         lower_model(ul_feed_dict["data"], **kwargs), ul_feed_dict["target"]
