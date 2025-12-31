8432f382a0a910857b26b6755403b661e4ee9c31
QHofmann <liuyaohua_918@163.com>
2025-12-31 14:50:07 +0800

    chore(boat_torch): remove deprecated code

--- boat_torch/dynamic_ol/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ngd import NGD
-from .dm import DM
-from .di import DI
-from .gda import GDA
-from .dynamical_system import DynamicalSystem
-from .sequential_ds import makes_functional_dynamical_system
