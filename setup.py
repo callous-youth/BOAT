84329e5ee10f9490d892f066266c993635917470
QHofmann <liuyaohua_918@163.com>
2025-12-31 15:04:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 current_directory = path.abspath(path.dirname(__file__))
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
@@ -15,9 +15,10 @@ def get_install_requirements():
 
 setup(
     name="boat-torch",
-    version="0.0.1",
+    version="1.0.2",
     packages=find_packages(),
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/callous-youth/BOAT",
     license="MIT",
     keywords=["Bilevel-optimization", "Learning and vision", "Python", "Deep learning", "PyTorch"],
@@ -26,10 +27,14 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8.0",
-    author="Yaohua Liu, Xianghao Jiao, Risheng Liu",
+    author="Yaohua Liu, Jibao Pan, Xianghao Jiao, Jiaxin Gao, Zhu Liu, Risheng Liu",
     author_email="liuyaohua.918@gmail.com",
-    description="Problem-Agnostic Bilevel Optimization Toolkit in Python for Learning and Vision Tasks",
+    description="BOAT: A Compositional Operation Toolbox for Gradient-based Bi-Level Optimization",
 )
